2d52dd62d8819e62c1058b4443c427f76d44a523
unknown <naoseioqbotaraq@gmail.com>
2025-05-27 13:45:07 -0300

    ajustes na main com testes manuais

--- Main.java
+++ Main.java
@@ -6,33 +6,40 @@ public class Main {
     public static void main(String[] args) {
         try {
             List<String> names = FileReader.readNames("resources/female_names.txt");
-            Collections.shuffle(names); // Embaralhar a lista
+            Collections.shuffle(names);
+            
+            HashTablePolynomial tablePolynomial = new HashTablePolynomial(32);
+            HashTableMultiplier tableMulti = new HashTableMultiplier(32);
 
-            HashTablePolynomial table = new HashTablePolynomial(32);
-            HashTableMultiplier multiTable = new HashTableMultiplier(32);
-
-
-            System.out.println("=== Inserindo Nomes ===");
-            int insertCount = Math.min(32, names.size()); //variavel que define quantos nomes serao inseridos na lista
-            for (int i = 0; i < insertCount; i++) {
-                String name = names.get(i);
-                boolean inserted = table.insert(name);
-                System.out.printf("Inserindo %s: %s\n", name, inserted ? "Sucesso" : "Falha");
+            System.out.println("Inserindo 40 nomes aleatórios na table");
+            for (int i = 0; i < 40; i++) {
+                tablePolynomial.insert(names.get(i));
+                tableMulti.insert(names.get(i));
             }
 
-            System.out.println("=== Inserindo Nomes ===");
-            int insertCountmulti = Math.min(32, names.size()); 
-            for (int i = 0; i < insertCountmulti; i++) {
-                String name = names.get(i);
-                boolean inserted = multiTable.insert(name);
-                System.out.printf("Inserindo %s: %s\n", name, inserted ? "Sucesso" : "Falha");
-            }
 
-            table.printStatistics();
-            multiTable.printStatistics();
+            //testes das operações insert, search e remove
+            String target = "Alice";
+            System.out.println("Inserção direta do nome: " + target);
+            System.out.println("Inserção Manual: Polynomial=" + tablePolynomial.insert(target) 
+            + ", Multiplier=" + tableMulti.insert(target));
+            System.out.println("Busca inicial: Polynomial=" + tablePolynomial.search(target) 
+                             + ", Multiplier=" + tableMulti.search(target));
+            
+            System.out.println("Removendo nome existente: Polynomial=" + tablePolynomial.remove(target) 
+                             + ", Multiplier=" + tableMulti.remove(target));
+            
+            System.out.println("Busca pós-remoção: Polynomial=" + tablePolynomial.search(target) 
+                             + ", Multiplier=" + tableMulti.search(target));
+                
+
+            System.out.println("\n===== Resultados =====");
+            tablePolynomial.printStatistics();
+            tableMulti.printStatistics();
 
         } catch (Exception e) {
-            System.err.println("Erro: " + e.getMessage());
+            System.err.println("Erro durante a execução: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
